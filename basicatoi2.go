aa6e6bc4f7c58ae633cf04611bab3aca11d5c6dc
daniel10027 <danielguedegbe10027@gmail.com>
2019-06-17 19:36:08 +0000

    atpi

---
+++ basicatoi2.go
@@ -0,0 +1,18 @@
+package piscine
+
+import ( 
+        
+        "strconv"
+
+)
+
+func BasicAtoi2(s string) int {
+            a := 0
+           intI, err := strconv.Atoi(s)
+	if err == nil{
+		 a = intI 
+}
+
+return a
+}
+
