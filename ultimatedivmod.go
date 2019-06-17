aa6fe6d1ba98260cf4961805389410d29e112ba4
daniel10027 <danielguedegbe10027@gmail.com>
2019-06-17 19:16:48 +0000

    ultim

---
+++ ultimatedivmod.go
@@ -0,0 +1,11 @@
+package piscine
+
+func UltimateDivMod(a *int, b *int) {
+                c := *a
+               *a = *a / *b
+                 
+               *b = c % *b
+
+                 
+}
+
