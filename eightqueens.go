1cb129b5c63be8cf004a58c1c4ffd17714bf4157
daniel10027 <danielguedegbe10027@gmail.com>
2019-06-20 18:31:07 +0000

    tas

---
+++ eightqueens.go
@@ -0,0 +1,38 @@
+
+package piscine
+
+import ("fmt")
+
+const numb = 8
+var pst = [numb]int{}
+
+func getStatus(qNbr , rPst int) bool{
+	for i := 0; i < qNbr; i++  {
+		orPst := pst[i]
+		if orPst == rPst || orPst == rPst - (qNbr - i) || orPst == rPst + (qNbr - i){
+			return false
+		}
+	}
+	return true
+}
+
+func resolve(q int){
+	if q == numb {
+		for i := 0; i < numb; i++ {
+			fmt.Print(pst[i] + 1)
+		}
+		fmt.Print("\n")
+	}else {
+		for i := 0; i < numb; i++ {
+			if getStatus(q, i) {
+				pst[q] = i
+				resolve(q + 1)
+			}
+		}
+	}
+}
+
+
+func EightQueens() {
+	resolve(0)
+}
